8df3cff91f9284b9fc708d6ebfed3311109811b1
tientoan <143169549+tientoan@users.noreply.github.com>
2023-10-02 17:34:21 +0700

    change some structure file

---
+++ src/java/course/CourseDAO.java
@@ -0,0 +1,10 @@
+
+package course;
+
+/**
+ *
+ * @author TienToan
+ */
+public class CourseDAO {
+    
+}
--- src/java/users/UserDAO.java
+++ src/java/users/UserDAO.java
@@ -5,7 +5,7 @@
  */
 package users;
 
-import DBUtils.DBUtil;
+import utils.DBUtil;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
