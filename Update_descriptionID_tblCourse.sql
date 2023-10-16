ad3740a18edfec5682173c214ada21756d0f251a
minhnguyet2308 <92633401+minhnguyet2308@users.noreply.github.com>
2023-10-16 16:43:36 +0700

    Update dữ liệu cột descriptionID bảng tblCourse

---
+++ Update_descriptionID_tblCourse.sql
@@ -0,0 +1,9 @@
+﻿
+-------- Update dữ liệu cột descriptionID bảng tblCourse -------
+
+UPDATE tblCourse SET descriptionID = 1 WHERE courseID = 'anidraw'
+UPDATE tblCourse SET descriptionID = 2 WHERE courseID = 'bdp1'
+UPDATE tblCourse SET descriptionID = 3 WHERE courseID = 'blender1'
+UPDATE tblCourse SET descriptionID = 4 WHERE courseID = 'portrait'
+UPDATE tblCourse SET descriptionID = 5 WHERE courseID = 'skp1'
+UPDATE tblCourse SET descriptionID = 6 WHERE courseID = 'vector1'
