85e61222abfdaf6e6b4cfb186095bca89a1ffdfa
Nguyen Tien Toan <143169549+tientoan@users.noreply.github.com>
2023-10-16 22:41:22 +0700

    Update Constants.java

--- src/java/userGoogle/Constants.java
+++ src/java/userGoogle/Constants.java
@@ -11,11 +11,13 @@ public class Constants {
 
     public static String GOOGLE_CLIENT_SECRET = "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";
 
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Wedproject2/UserGoogleHandler";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Wedproject2_temp1/UserGoogleHandler";
 
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
 
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
+    
+    
 }
