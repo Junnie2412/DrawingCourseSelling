fdf9b76fcc5672fe81e0c73b397614aaa499765c
tientoan <toanntse171537@fpt.edu.vn>
2023-10-31 16:51:24 +0700

    fix forgot password

---
+++ src/java/controllers/forgotpassword/SendMail.java
@@ -0,0 +1,48 @@
+package controllers.forgotpassword;
+import javax.mail.*;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Win 10
+ */
+public class SendMail {
+
+    public static boolean send(String to, String sub,
+            String msg) {
+        boolean check = false;
+        String user = "artcenter2412@gmail.com";
+        String pass = "rduaiicjoamzvsfo";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(sub);
+            message.setContent(msg, "text/html");
+            Transport.send(message);
+            check = true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return check;
+    }
+
+}
