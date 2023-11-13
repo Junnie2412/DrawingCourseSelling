81f8ee0ddf2e63e7dc0fc13a38220c6aaa3f9607
Nguyen Hoang My Phuoc <phuocnhmse171617@fpt.edu.vn>
2023-11-13 19:09:54 +0700

    deactive course (ch xong)

--- src/java/controllers/staff/DeactiveCourseController.java
+++ src/java/controllers/staff/DeactiveCourseController.java
@@ -20,29 +20,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "DeactiveCourseController", urlPatterns = {"/DeactiveCourseController"})
 public class DeactiveCourseController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private static final String SUCCESS = "LoadCourseController";
+    private static final String ERROR = "LoadCourseController";
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DeactiveCourseController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DeactiveCourseController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String url = ERROR;
+        try{
+            String courseID = request.getParameter("courseID");
+            
+        }catch(Exception e){
+            log("Error at DeactiveCourseController: " + e.toString());
+        }finally{
+            request.getRequestDispatcher(url).forward(request, response);
         }
     }
 
